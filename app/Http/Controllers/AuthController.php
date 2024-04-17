8c93b3a86f8def9020f2873717753809ef81993c
Jeannie <jeanniebeth.commude@gmail.com>
2024-04-17 09:42:27 +0800

    Initial commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'username' => 'required|string|unique:users,username|max:255',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        return 'test';
+
+        $user = User::create(array_merge(
+            $validatedData,
+            ['password' => bcrypt($request->password)]
+        ));
+
+        $token = $user->createToken('authToken')->plainTextToken;
+
+        return response()->json(['token' => $token], 201);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        if (!Auth::attempt($request->only('username', 'password'))) {
+            throw ValidationException::withMessages([
+                'username' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $token = $request->user()->createToken('authToken')->plainTextToken;
+
+        return response()->json(['token' => $token], 200);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Logged out'], 200);
+    }
+}
+
---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'username' => ['required', 'exists:users,username'],
+            'password' => ['required','min:8'],
+        ];
+    }
+
+    public function data(): array
+    {
+        $data = $this->only('username');
+
+        if (! is_null($this->input('password'))) {
+            $data['password'] = bcrypt($this->password);
+        }
+
+        return $data;
+    }
+}
